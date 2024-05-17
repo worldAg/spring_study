3b8db307539e388800ef6623dfc17866a9c95062
worldAg <worldag17@gmail.com>
2024-05-17 22:20:57 +0900

    Update Hotel.java
    생성자 자동 주입 및 Lombok을 결합한 Chef 주입(@AllArgsConstructor)

--- ex01_setting_xml/src/main/java/com/hello/example/Hotel.java
+++ ex01_setting_xml/src/main/java/com/hello/example/Hotel.java
@@ -2,6 +2,7 @@ package com.hello.example;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -11,13 +12,14 @@ import lombok.ToString;
 @Component
 @ToString
 @Getter
+@AllArgsConstructor // 인스턴스 변수로 선언된 모든 것을 파라미터로 받는 생성자 작성
 public class Hotel {
 	
 	private Chef chef;
 	
 	// @Autowired 없이 생성자 선언 후 Chef 주입
-	public Hotel(Chef chef) {
-		this.chef = chef;
-	}
+//	public Hotel(Chef chef) {
+//		this.chef = chef;
+//	}
 	
 }
