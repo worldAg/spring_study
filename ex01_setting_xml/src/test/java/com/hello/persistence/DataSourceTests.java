c0fe85a1764ae8b82a7d54e0eb780d4bf8e278db
worldAg <worldag17@gmail.com>
2024-05-24 23:56:12 +0900

    Update DataSourceTests.java
    SqlSessionFactoryBean을 이용한 SqlSession 사용 테스트

--- ex01_setting_xml/src/test/java/com/hello/persistence/DataSourceTests.java
+++ ex01_setting_xml/src/test/java/com/hello/persistence/DataSourceTests.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -19,17 +21,30 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class DataSourceTests {
-	
+
 	@Setter(onMethod_ = { @Autowired })
 	private DataSource dataSource;
-	
+
+	@Setter(onMethod_ = { @Autowired })
+	private SqlSessionFactory sqlSessionFactory;
+
 	@Test
 	public void testConnection() {
 		try (Connection con = dataSource.getConnection()) {
 			log.info(con);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 	}
-	
+
+	@Test
+	public void testMyBatis() {
+		try (SqlSession session = sqlSessionFactory.openSession(); Connection con = session.getConnection();) {
+			log.info(session);
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
 }
